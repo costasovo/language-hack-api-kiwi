bd50f9e8c855228d6dc09e996abb64371a4dc631
Rostislav Wolny <rostislav.wolny@sygictravel.com>
2016-11-12 15:43:15 +0100

    Hacked easy jet

--- graphql/database/database/getAirlineName.js
+++ graphql/database/database/getAirlineName.js
@@ -1,8 +1,21 @@
 import request from 'request';
 
+var searches = {};
+
 // Note: This is where your code should go to fetch a real user.
 export default (code) => {
-	return new Promise(function (resolve, reject) {
+	if (searches[code]) {
+		return searches[code];
+	}
+	searches[code] = new Promise(function (resolve, reject) {
+		if (code === 'U2') {
+			resolve('easyJet');
+			return;
+		}
+		if (code === 'FR') {
+			resolve('Ryanair');
+			return;
+		}
 		const url = 'https://iatacodes.org/api/v6/airlines?api_key=e6d744e0-ab38-4e3b-a0c0-9c4e8c8a537d&code=' + code;
 		request(url, function (error, response, body) {
 			if (!error && response.statusCode == 200) {
@@ -17,4 +30,5 @@ export default (code) => {
 			}
 		});
 	});
+	return searches[code];
 };
